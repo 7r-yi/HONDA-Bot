06cab182b31072fb8e87a86f3def5be0ecc5b824
7r-yi <Afyx-rily@outlook.jp>
2020-11-02 03:10:47 +0900

    Update

--- zyanken/zyanken.py
+++ zyanken/zyanken.py
@@ -171,7 +171,7 @@ def ranking_output(type, guild):
         j, k, = 1, 0
         for i in range(len(sort_data)):
             stc += f"{j}位 : {guild.get_member(sort_data[i][0]).display_name} " \
-                   f"({sort_data[i][6]}点, 勝率{sort_data[i][3]:.02f}%, {sort_data[i][5]}連勝中)"
+                   f"({sort_data[i][6]}点, 勝率{sort_data[i][3]:.02f}%, {sort_data[i][4]}連勝中)"
             if i >= len(sort_data) - 2:  # ワースト2場合Loser
                 stc += " [Loser]"
             stc += "\n"
