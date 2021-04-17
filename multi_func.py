a39fd61218d3bcacca3dd260c45fde2ba3177d33
7r-yi <Afyx-rily@outlook.jp>
2021-04-18 01:05:52 +0900

    Update

--- multi_func.py
+++ multi_func.py
@@ -1,5 +1,5 @@
 import discord
-import constant
+import constant as cs
 
 
 def get_role(guild, role_id):
@@ -7,14 +7,14 @@ def get_role(guild, role_id):
 
 
 def role_check_admin(ctx_role):
-    return constant.Administrator in [roles.id for roles in ctx_role.author.roles]
+    return cs.Administrator in [roles.id for roles in ctx_role.author.roles]
 
 
 def role_check_mode(ctx_role):
     roles = [roles.id for roles in ctx_role.author.roles]
-    return any([constant.Administrator in roles, constant.Moderator in roles])
+    return any([cs.Administrator in roles, cs.Moderator in roles])
 
 
 def role_check_visit(ctx_role):
     roles = [roles.id for roles in ctx_role.author.roles]
-    return any([constant.Administrator in roles, constant.Moderator in roles, constant.Visitor in roles])
+    return any([cs.Administrator in roles, cs.Moderator in roles, cs.Visitor in roles])
